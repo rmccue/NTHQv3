4c2ec62a7f69af9bf4fcf165fe87aa23f40905ea
Ryan McCue <ryanmccue@cubegames.net>
2010-03-07 12:51:39 +1000

    Hide titles on pages

--- page.php
+++ page.php
@@ -5,15 +5,13 @@
 <?php the_post(); ?>
 
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<!--<h1 class="entry-title"><?php the_title(); ?></h1>-->
 					<div class="entry-content">
 						<?php the_content(); ?>
-						<?php wp_link_pages( 'before=<div class="page-link">' . __( 'Pages:', 'twentyten' ) . '&after=</div>'); ?>
-						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+						<?php wp_link_pages( 'before=<div class="page-link">' . __( 'Pages:', 'nthq' ) . '&after=</div>'); ?>
+						<?php edit_post_link( __( 'Edit Page', 'nthq' ), '<div class="edit-link block">', '</div>' ); ?>
 					</div><!-- .entry-content -->
 				</div><!-- #post-<?php the_ID(); ?> -->
-
-				<?php comments_template( '', true ); ?>
 
 			</div><!-- #main -->
 
